e05d198777a6a283cfe0c6e8e4b724d8d6786c37
spacemandev <devbharel@gmail.com>
2021-02-23 22:54:52 -0600

    shifting computers

--- tests/upload.test.ts
+++ tests/upload.test.ts
@@ -1,3 +1,14 @@
-test('Prints Hello World', () => {
-  console.log("Hello World")
-})
+import { fstat } from "fs";
+
+import * as fs from 'fs';
+import * as fetch from 'node-fetch';
+
+test('Fetch upload preflight from server', () => {
+  //Read file from disk
+  let map = fs.readFileSync('./upload_test/Mountain_Dig_Site.png');
+  console.log("Map File: ", map);
+
+  //Send metadata along with file to server for preflight
+  
+});
+
