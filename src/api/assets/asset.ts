e09ab7e86f2a6efc303fa023b4c7d4fb557f29e6
Vindexus <vindexus@gmail.com>
2021-04-01 12:54:36 -0700

    Made some small adjustments to the code structure.

--- src/api/assets/asset.ts
+++ src/api/assets/asset.ts
@@ -1,108 +1,135 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import {
+  APIGatewayEventDefaultAuthorizerContext,
+  APIGatewayProxyEventBase,
+  APIGatewayProxyHandler,
+  Context
+} from 'aws-lambda';
 import { search } from '../common/elastic';
-import { Asset, image_file_resolutions, REQ_Query, visibility_types } from '../../interfaces/IAsset';
+import { Asset, image_file_resolutions, REQ_Query } from '../../interfaces/IAsset';
 import * as b2 from '../common/backblaze';
 import { dyn } from '../common/database';
 //import { dyn } from '../common/database';
 //import { User } from '../../interfaces/IUser';
 
-export const query_assets: APIGatewayProxyHandler = async (_evt, _ctx) => {
-  let response = {
+function newResponse () {
+  return {
     statusCode: 500,
     headers: {
       'content-type': "application/json",
-      'Access-Control-Allow-Origin': "*",
-      'Access-Control-Allow-Credential': true
+        'Access-Control-Allow-Origin': "*",
+        'Access-Control-Allow-Credential': true
     },
     body: JSON.stringify({error:"Something went wrong!"})
   }
+}
+
+function transformAsset(FrontEndAsset : Asset) : Asset {
+  FrontEndAsset.creatorName = FrontEndAsset.creatorID; //TODO change to above code when we have users
+  FrontEndAsset.previewLink = b2.GetURL('watermarked', FrontEndAsset);
+  FrontEndAsset.thumbnail =  b2.GetURL('thumbnail', FrontEndAsset);
+  return FrontEndAsset
+}
+
+const query_asset = async (evt: APIGatewayProxyEventBase<APIGatewayEventDefaultAuthorizerContext>, ctx : Context) => {
+  let doc;
+  const response = newResponse()
+  const id = evt.queryStringParameters['id']
+  try{
+    doc = await search.get({
+      index: process.env.INDEX_ASSETDB,
+      id: id
+    })
+  } catch (e) {
+    // Doc doesn't exist
+    response.body = JSON.stringify({error: `${id} doesn't exist in Index`});
+    throw new Error(`${id} doesn't exist in Index`);
+  }
+  let FrontEndAsset:Asset = doc.body._source;
+  /*
+	FrontEndAsset.creatorName = (<User>(await dyn.get({
+		TableName: process.env.NAME_USERSDB,
+		Key: {
+			id: FrontEndAsset.creatorID
+		}
+	}).promise()).Item).name;
+	*/
+  FrontEndAsset = transformAsset(FrontEndAsset)
+  response.body = JSON.stringify(FrontEndAsset);
+  response.statusCode = 200;
+  return response;
+}
+
+export const query_assets: APIGatewayProxyHandler = async (_evt, _ctx) => {
+  const response = newResponse()
 
   try{
     let queryObj:REQ_Query = {};
-    // Create lists from comma deliminted query string parameters
-    for(let key of Object.keys(_evt.queryStringParameters)){
-      let val = _evt.queryStringParameters[key].split(",")
-      queryObj[key] = val.length == 1 ? val[0] : val 
+    if (_evt.queryStringParameters) {
+      // Create lists from comma deliminted query string parameters
+      for(let key of Object.keys(_evt.queryStringParameters)){
+        let val = _evt.queryStringParameters[key].split(",")
+        queryObj[key] = val.length == 1 ? val[0] : val
+      }
     }
 
     // If ID then just do a GET on the ID, search params don't matter
     if(queryObj['id']){
-      let doc;
-      try{
-        doc = await search.get({
-          index: process.env.INDEX_ASSETDB,
-          id: queryObj['id']
-        })  
-      } catch (e) {
-        // Doc doesn't exist
-        response.body = JSON.stringify({error: `${queryObj['id']} doesn't exist in Index`});
-        throw new Error(`${queryObj['id']} doesn't exist in Index`);
-      }
-      let FrontEndAsset:Asset = doc.body._source;
-      /*
-      FrontEndAsset.creatorName = (<User>(await dyn.get({
-        TableName: process.env.NAME_USERSDB,
-        Key: {
-          id: FrontEndAsset.creatorID
-        }
-      }).promise()).Item).name;
-      */
-      FrontEndAsset.creatorName = FrontEndAsset.creatorID; //TODO change to above code when we have users
-      FrontEndAsset.previewLink = b2.GetURL('watermarked', FrontEndAsset);
-      FrontEndAsset.thumbnail =  b2.GetURL('thumbnail', FrontEndAsset);
-      //FrontEndAsset.previewLink = `https://f000.backblazeb2.com/file/advl-watermarked/${FrontEndAsset.creatorID}/${FrontEndAsset.id}.webp`
-      //FrontEndAsset.thumbnail = `https://f000.backblazeb2.com/file/advl-watermarked/${FrontEndAsset.creatorID}/${FrontEndAsset.id}.webp`    
-      response.body = JSON.stringify(FrontEndAsset);
-      response.statusCode = 200;
-      return response;  
+      return await query_asset(_evt, _ctx)
     }
 
-    let _query = {
-      "dis_max": {
-        "queries": [],
-        "tie_breaker": 0.7
-      }
-    }
+    let _query : any = {}
+    const queries = []
+    let exclude_attributes = ['sort', 'sort_type', 'from', 'size', 'text']
     for(let key of Object.keys(queryObj)){
+      const val = queryObj[key]
       //id key is already taken care of in the above code block
-      let exclude_attributes = ['sort', 'sort_type', 'from', 'size', 'text']
       if(!exclude_attributes.includes(key)){
-        _query.dis_max.queries.push({
+        queries.push({
           "match": {
-            key: queryObj[key]
+            key: val
           }
         })
       } else if(key == 'text'){
-        _query.dis_max.queries.push({
+        queries.push({
           "match": {
-            'name': queryObj[key]
+            'name': val
           }
         })
-        _query.dis_max.queries.push({
+        queries.push({
           "match": {
-            'description': queryObj[key]
+            'description': val
           }
         })
       }
     }
-    
+
     if(_query.dis_max.queries.length == 0){
       throw new Error("Query string was empty");
     }
 
+    if (queries.length) {
+      _query.dis_max = {
+        queries: queries,
+        "tie_breaker": 0.7
+      }
+    }
+
+    const body = {
+      from: queryObj['from'] ? queryObj['from'] : 0,
+      size: queryObj['size'] ? queryObj['size'] : 10,
+      sort: queryObj['sort'] ? [{
+        [queryObj['sort']] : queryObj['sort_type']
+      }] : [{
+        "_score": "desc"
+      }],
+      query: _query
+    }
+    console.log('body', body)
+
     // Query doesn't include ID
     let searchResults = await search.search({
       index: process.env.INDEX_ASSETDB,
-      body: {
-        from: queryObj['from'] ? queryObj['from'] : 0,
-        size: queryObj['size'] ? queryObj['size'] : 10,
-        sort: queryObj['sort'] ? [{
-          [queryObj['sort']] : queryObj['sort_type']
-        }] : [{
-          "_score": "desc"
-        }],
-        query: _query
-      }
+      body: body
     })
 
     let FrontEndAssets:Asset[] = searchResults.body.hits.hits.map((doc:any) => {
@@ -114,17 +141,14 @@ export const query_assets: APIGatewayProxyHandler = async (_evt, _ctx) => {
         }
       }).promise()).Item).name;
       */
-      doc._source.creatorName = doc._source.creatorID; //TODO change to above code when we have users
-      doc._source.previewLink = b2.GetURL('watermarked', doc._source);
-      doc._source.thumbnail = b2.GetURL('thumbnail', doc._source);
-      //doc._source.previewLink = `https://f000.backblazeb2.com/file/advl-watermarked/${doc._source.creatorID}/${doc._source.id}.webp`
-      //doc._source.thumbnail = `https://f000.backblazeb2.com/file/advl-watermarked/${doc._source.creatorID}/${doc._source.id}.webp`    
+      doc._source = transformAsset(doc._source)
       return doc._source
     })
     response.body = JSON.stringify(FrontEndAssets);
     response.statusCode = 200;
     return response;
   } catch (E){
+    console.error(E)
     console.error(`ERROR | \n Event: ${_evt} \n Error: ${E}` );
     return response;
   }
@@ -143,7 +167,7 @@ export const asset_download_link: APIGatewayProxyHandler = async (_evt, _ctx) =>
 
   try{
     let doc;
-    try{ 
+    try{
       doc = await search.get({
         index: process.env.INDEX_ASSETDB,
         id: _evt.queryStringParameters.id
@@ -184,7 +208,7 @@ export const update_asset: APIGatewayProxyHandler = async (_evt, _ctx) => {
       doc = await search.get({
         index: process.env.INDEX_ASSETDB,
         id: reqAsset['id']
-      })  
+      })
     } catch (e) {
       // Doc doesn't exist
       response.body = JSON.stringify({error: `${reqAsset['id']} doesn't exist in Index`});
