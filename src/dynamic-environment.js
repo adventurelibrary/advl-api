ee180ad9bdae13abeff614e9087fe44fbbbc80f1
spacemandev <devbharel@gmail.com>
2021-05-02 18:59:22 -0500

    updated dynamic environments

--- src/dynamic-environment.js
+++ src/dynamic-environment.js
@@ -4,20 +4,30 @@ const env = YAML.load('src/.env.yml');
 module.exports = {
   transloadit: {
     ...env.transloadit,
-    ...env.elastic
+    ...env.elastic,
+    ...env.postgres,
+    ...env.db
   },
   asset_download_link: {
     ...env.elastic,
-    ...env.backblaze
+    ...env.backblaze,
+    ...env.postgres,
+    ...env.db
   },
   query_assets: {
     ...env.elastic,
-    ...env.backblaze
+    ...env.backblaze,
+    ...env.postgres,
+    ...env.db
   },
   user: {
-    ...env.elastic
+    ...env.elastic,
+    ...env.postgres,
+    ...env.db
   },
   creator: {
-    ...env.elastic
+    ...env.elastic,
+    ...env.postgres,
+    ...env.db
   }
 }
