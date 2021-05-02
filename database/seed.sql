f62cc49f634e8942d79fdf1ecea0bc15c33e8de1
Vindexus <vindexus@gmail.com>
2021-05-01 19:43:00 -0700

    Update seed file

--- database/seed.sql
+++ database/seed.sql
@@ -1,5 +1,45 @@
-INSERT INTO creators (id, slug, name, description) VALUES
-('5caa7fd3-48aa-47fb-a7f1-f0e7b32b1721', 'default-creator', 'Default Creator', 'Just a default value');
 
-INSERT INTO assets (id, creator_id, slug, name, description, tags, original_file_ext, type, size_in_bytes) VALUES
-('436b14c1-07e1-402c-abfd-70140d3d4e27', '5caa7fd3-48aa-47fb-a7f1-f0e7b32b1721', 'cool-token', 'Cool Token', '', '{"Archer","Winter"}', 'jpg', 'TOKEN', 0);
+INSERT INTO creators (id, name, slug) VALUES
+('5f9f10e7-5002-444f-bf24-562e218e971d', 'Adventure Library', 'adventure-library');
+
+INSERT INTO assets (id, creator_id, slug, name, description, tags, original_file_ext, type, visibility, size_in_bytes) VALUES
+
+('8fb5fb80-9177-49e3-81d0-2114d861fa18','5f9f10e7-5002-444f-bf24-562e218e971d', 'knightly-camp-of-fire-7195', 'Knightly Camp of Fire', 'ever ever produces pleasure? happiness. pleasure laborious To that no because man anyone him again pain born give a and ', '{"Priest","Fall","Fantasy"}', 'webp','CHARACTER', 'PUBLIC', 5990056),
+
+('ecbadbcc-b8fa-47ff-a3e2-d32ef47534c7','5f9f10e7-5002-444f-bf24-562e218e971d', 'ashen-cathedral-of-orcs-2123', 'Ashen Cathedral of Orcs', 'truth, extremely system, you the or occasionally complete denouncing of loves explorer how who with mistaken is has no who praising ', '{"Archer","Town"}', 'webp','SCENE', 'PUBLIC', 6026522),
+
+('4fed2771-5073-4bd8-95d4-c457f1e5c74a','5f9f10e7-5002-444f-bf24-562e218e971d', 'wartorn-cathedral-of-doom-6424', 'Wartorn Cathedral of Doom', 'painful. is praising system, circumstances can but which great pain you expound to there rejects, pain no again of how consequences there explain obtain again ', '{"Castle"}', 'webp','CHARACTER', 'PUBLIC', 6019089),
+
+('ec2c56fc-3281-45fe-9c5a-9bf098c0a74f','5f9f10e7-5002-444f-bf24-562e218e971d', 'sunny-glade-of-the-king-7726', 'Sunny Glade of the King', 'the of born exercise, the physical in produces because pleasure him consequences rejects, No or because toil pleasure. to who this the ', '{"Town","Archer","Orc","Winter"}', 'jpg','CHARACTER', 'PUBLIC', 6088000),
+
+('695c57c1-fc7e-4cc8-821e-0f7869118f10','5f9f10e7-5002-444f-bf24-562e218e971d', 'windy-forest-of-the-vampires-4769', 'Windy Forest of the Vampires', 'except do account the pursue ever toil the those denouncing from is how and or pleasure pain annoying To again because pleasure great one ', '{"Archer"}', 'webp','MAP', 'PUBLIC', 5917470),
+
+('eaf77b3c-a856-4728-b110-38462cf26e4c','5f9f10e7-5002-444f-bf24-562e218e971d', 'holy-fortress-of-the-vampires-4427', 'Holy Fortress of the Vampires', 'But the pursues give or painful. human to To again painful. to painful. has circumstances account I resultant of and pleasure ', '{"Castle","Orc"}', 'webp','MAP', 'HIDDEN', 5732569),
+
+('d9131a29-6a9b-439e-9c61-33ae344698fd','5f9f10e7-5002-444f-bf24-562e218e971d', 'frozen-fort-of-fire-7598', 'Frozen Fort of Fire', 'or I a pain man man human pleasure, procure pleasure? rationally there the pain undertakes pain pain, there truth, pursue consequences, ', '{"Fantasy"}', 'webp','CHARACTER', 'PUBLIC', 6069975),
+
+('f55f0bb6-65e6-4cef-8f37-237f00ac0c8f','5f9f10e7-5002-444f-bf24-562e218e971d', 'knightly-camp-of-ice-472', 'Knightly Camp of Ice', 'obtain a know that great a and it Nor not pursues the a system, has painful. extremely of to and pursue great to give who some those who ', '{"Fantasy","Priest","Fall","Archer"}', 'webp','MAP', 'PUBLIC', 5857029),
+
+('7d445c1c-ba81-49cf-8ac1-8e8df7a86e9b','5f9f10e7-5002-444f-bf24-562e218e971d', 'sunny-oasis-of-fire-6676', 'Sunny Oasis of Fire', 'not a it pain a in of I is Nor there know chooses again happiness. I But or not how who pain of to that fault ', '{"Winter","Priest"}', 'webp','TOKEN', 'PUBLIC', 5897753),
+
+('6b5820e8-f308-4560-8698-f27a47d004fd','5f9f10e7-5002-444f-bf24-562e218e971d', 'wartorn-camp-of-dawn-9265', 'Wartorn Camp of Dawn', 'find of rationally human a to of do trivial consequences But because mistaken pleasure to who in itself, who pleasure? exercise, born it? enjoy pain has pleasure exercise, the one ', '{"Town"}', 'webp','TOKEN', 'PUBLIC', 5843708),
+
+('5ef6df5b-da80-4be1-8c1d-ee066e9beec6','5f9f10e7-5002-444f-bf24-562e218e971d', 'frozen-town-of-doom-7135', 'Frozen Town of Doom', 'must or right must of you exercise, the you pleasure toil painful. idea pleasure. that there rejects, is happiness. some ', '{"Spring","Town"}', 'webp','SCENE', 'PUBLIC', 6092825),
+
+('f28165c6-eaad-44d6-8e7c-332ff963d22b','5f9f10e7-5002-444f-bf24-562e218e971d', 'windy-cathedral-of-fire-6862', 'Windy Cathedral of Fire', 'But know explain laborious pursue to rationally pain in and can some because it a has give explain one pursue ', '{"Winter","Spring","Fantasy"}', 'webp','SCENE', 'PUBLIC', 5867439),
+
+('c3e523c8-fef9-47a1-8ade-3928d348e8af','5f9f10e7-5002-444f-bf24-562e218e971d', 'holy-forest-of-fire-3943', 'Holy Forest of Fire', 'the loves truth, pleasure explain born must will with avoids must except I is or how to But teachings pleasure. any from teachings ', '{"Winter","Castle"}', 'webp','SCENE', 'PUBLIC', 5862511),
+
+('c2da1bb0-9946-42c5-9146-cb16083c44ca','5f9f10e7-5002-444f-bf24-562e218e971d', 'sunny-camp-of-dawn-1663', 'Sunny Camp of Dawn', 'one occasionally Nor of the But again pleasure? consequences, can of anyone produces us a it? system, dislikes, toil all him ', '{"Castle","Summer"}', 'webp','MAP', 'PUBLIC', 5750534),
+
+('34e6f93f-25c3-4158-9c52-188733f227c6','5f9f10e7-5002-444f-bf24-562e218e971d', 'frozen-temple-of-orcs-6820', 'Frozen Temple of Orcs', 'it man expound man who is occasionally the encounter occur avoids because to a do a who loves I expound ', '{"Town","Winter"}', 'webp','TOKEN', 'PUBLIC', 5720124),
+
+('3556fb36-5efc-453a-8783-f3a25a25cc72','5f9f10e7-5002-444f-bf24-562e218e971d', 'frozen-fortress-of-fire-8542', 'Frozen Fortress of Fire', 'some anyone because anyone to has to man because which itself, great take a and mistaken great pleasure and denouncing pleasure. rationally explain account But ', '{"Village","Castle"}', 'jpg','TOKEN', 'PUBLIC', 6087411),
+
+('d5b2f75b-6838-41e7-8f73-9abd47b93b24','5f9f10e7-5002-444f-bf24-562e218e971d', 'windy-oasis-of-ice-1262', 'Windy Oasis of Ice', 'a procure pleasure. pain, to painful. consequences advantage account itself, of To the and of mistaken and itself, pain but obtain will account human that of produces consequences, avoids ', '{"Priest"}', 'webp','CHARACTER', 'PUBLIC', 6010670),
+
+('6e0c3a32-b752-47b6-b8d0-8f60f1ed689e','5f9f10e7-5002-444f-bf24-562e218e971d', 'ashen-camp-of-orcs-4964', 'Ashen Camp of Orcs', 'but anyone pursue which is that of occur the expound has great truth, explorer and pain itself, has pursues do who a and is extremely and Nor a ', '{"Barbarian","Village","Orc"}', 'webp','TOKEN', 'HIDDEN', 6110797),
+
+('bfed82c2-e879-41c7-bdc8-03c0dfe48e29','5f9f10e7-5002-444f-bf24-562e218e971d', 'holy-town-of-the-queen-8775', 'Holy Town of the Queen', 'is who pleasure of must pleasure of occasionally because must extremely exercise, to encounter because not a a exercise, a of painful. Nor ', '{"Fall","Winter"}', 'webp','MAP', 'PUBLIC', 5800851),
+
+('65dbb04c-3297-4a39-8088-d9371410fcc8','5f9f10e7-5002-444f-bf24-562e218e971d', 'sunny-fort-of-the-king-4439', 'Sunny Fort of the King', 'exercise, desires pain that who who explorer which obtain will and give No trivial denouncing because to pleasure is avoids ', '{"Winter","Fall","Priest"}', 'jpg','SCENE', 'PENDING', 5820501);
